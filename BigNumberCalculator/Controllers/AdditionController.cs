4d8edda34635920798ea9e7a63f5a1f7e77b40b1
sayedur.rahmans <sayedur.rahmans@outlook.com>
2019-10-19 02:40:38 +0600

    Initial comit 1. asp.net core 2 wep api application created 2. Repo created

---
+++ BigNumberCalculator/Controllers/AdditionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigNumberCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
